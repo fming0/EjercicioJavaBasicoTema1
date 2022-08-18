dc596c01c99611fbb471b897e7117499860f8a04
Fabio Mingo <fabio020mingo@gmail.com>
2022-08-18 15:01:45 -0400

    Ejercicio Terminado

---
+++ src/main/java/miPaquete/MiClaseMain.java
@@ -0,0 +1,16 @@
+package miPaquete;
+
+public class MiClaseMain {
+    public static void main(String[] args) {
+        int numero1 = 5;
+        boolean miBooleano= true;
+        long numero2 = 3080008367L;
+        double numero3 = 3.14159;
+        String nombre ="Fabio";
+        System.out.println(numero1);
+        System.out.println(miBooleano);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(nombre);
+    }
+}
